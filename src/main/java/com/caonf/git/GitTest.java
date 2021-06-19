2dd5b3aecf8554c413106fcee0763688a0cf79b9
cningfeng <caonife@126.com>
2021-06-19 13:19:35 +0800

    second commit

--- src/main/java/com/caonf/git/GitTest.java
+++ src/main/java/com/caonf/git/GitTest.java
@@ -3,5 +3,6 @@ package com.caonf.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!!!");
+        System.out.println("hello git2!!!");
     }
 }
